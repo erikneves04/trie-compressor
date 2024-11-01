99048c3bc03c4ff3637841d6e399870a0edd1742
Erik Neves <erikrrn04@gmail.com>
2024-10-31 21:10:57 -0300

    add initial files

---
+++ src/Trie/Trie.py
@@ -0,0 +1,12 @@
+class Trie:
+    def __init__(self):
+        pass
+
+    def Insert(self, data):
+        pass
+
+    def Search(self, data):
+        pass
+
+    def Remove(self, data):
+        pass
