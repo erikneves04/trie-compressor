adea00e5252651117ebecf3d442746543d03bcb8
Erik Neves <erikrrn04@gmail.com>
2024-11-01 19:50:04 -0300

    rename trie unit test file

---
+++ tests/TestTrie.py
@@ -0,0 +1,8 @@
+import unittest
+from src.Trie.Trie import Trie
+
+class TestTrie(unittest.TestCase):
+    def setUp(self):
+        self.trie = Trie()
+        
+    # TODO: testes de unidade para a Trie
