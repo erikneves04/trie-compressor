cfca863f5e716a93a1b0703ec4b2418615f233f7
Erik Neves <erikrrn04@gmail.com>
2024-11-01 19:49:39 -0300

    add trie initialization

--- src/Trie/Trie.py
+++ src/Trie/Trie.py
@@ -1,12 +1,31 @@
 class Trie:
     def __init__(self):
-        pass
+        self.root = _trieNode(None, None, None)
 
     def Insert(self, data):
         pass
 
     def Search(self, data):
-        pass
+        return True
 
     def Remove(self, data):
         pass
+
+class _trieNode:
+    def __init__(self, left, right, top):
+        self.left = left
+        self.right = right
+        self.top = top
+
+        self.value = None
+        self.isLeaf = False
+
+    def IsLeaf(self):
+        return self.isLeaf
+
+    def GetValue(self):
+        return self.value
+
+    def SetValue(self, value):
+        self.value = value
+        self.isLeaf = True
