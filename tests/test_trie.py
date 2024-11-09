6c305e9bb0a47cb524be52fb8571054cc2aacb93
Gabrielzzz7 <gabrielcamposprudente19@gmail.com>
2024-11-09 16:00:59 -0300

    feat: initial tests for Trie

--- tests/test_trie.py
+++ tests/test_trie.py
@@ -13,4 +13,27 @@ def test_empty_trie_has_depth_one(trie):
     depth = trie.GetDepth()
     assert depth == 1
 
-# TODO: Add more unit tests for the Trie
+def test_insert_str_bin_trivial_case(trie):
+    strInsert = trie.Insert("000", 1)   
+    
+    assert strInsert[1] == "000" and strInsert[2] == "000" and strInsert[3] == 1
+    
+def test_insert_two_strings_with_different_prefixes(trie):
+    trie.Insert("000", 1)
+    strInsertTwo = trie.Insert("111", 2)
+    
+    assert strInsertTwo[1] == "111" and strInsertTwo[2] == "111" and strInsertTwo[3] == 2
+    
+def test_insert_two_with_common_prefix(trie):
+     trie.Insert("00000", 1)
+     strInsertTwo = trie.Insert("00011", 2)
+     
+     assert strInsertTwo[0] == "2 nós criados"
+     assert strInsertTwo[1] == "11"
+     assert strInsertTwo[2] == 2
+     assert strInsertTwo[3] == "00"
+     assert strInsertTwo[4] == 1   
+     
+       
+
+    
