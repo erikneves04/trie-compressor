1328fe526902a3a5007ecdbcb7f9c2b3c5912aac
Gabrielzzz7 <gabrielcamposprudente19@gmail.com>
2024-11-09 16:14:21 -0300

    fix: rename of variables

--- src/Trie/Trie.py
+++ src/Trie/Trie.py
@@ -25,8 +25,6 @@ class _trieNode:
 
     def GetSubstring(self):
         return self.substring
-
-
 class Trie:
     def __init__(self):
         self.root = _trieNode(None)
@@ -35,7 +33,7 @@ class Trie:
 
     def Insert(self, key, value):
         currentNode = self.root
-        deathInsetion = 0
+        insertionDepth = 0
         
         i = 0
         bit = int(key[i])
@@ -43,38 +41,38 @@ class Trie:
         if currentNode.children[bit] is None:
             currentNode.children[bit] = _trieNode(currentNode, key, value)
             currentNode = currentNode.children[bit]
-            deathInsetion += 1
+            insertionDepth += 1
             self._nodeCount += 1
-            self._depth = max(self._depth, deathInsetion)
+            self._depth = max(self._depth, insertionDepth)
             return (True, currentNode.GetSubstring(), key, value)
 
         while i < len(key):
-            child: _trieNode = currentNode.children[bit]
+            childNode : _trieNode = currentNode.children[bit]
             substring = child.GetSubstring()
             lenSubstring = len(substring)
 
             for j in range(lenSubstring):
                 if i + j >= len(key) or key[i + j] != substring[j]:
-                    prefixCurrentNode = substring[:j]
-                    sufixCurrentNode = substring[j:]
-                    sufixNewNode = key[i+j:]
+                    prefixMatch = substring[:j]
+                    suffixExisting = substring[j:]
+                    suffixNew = key[i+j:]
                     
                     breakbitKey = int(key[i + j])
                     breakbitSubstring = 0 if breakbitKey == 1 else 1
 
-                    child.SetSubstring(prefixCurrentNode)
-                    childKey = child.children[breakbitKey] = _trieNode(child, sufixNewNode, value)
-                    childSubsting = child.children[breakbitSubstring] = _trieNode(child, sufixCurrentNode, child.GetValue())
-                    deathInsetion += 1
+                    child.SetSubstring(prefixMatch)
+                    childKey = child.children[breakbitKey] = _trieNode(child, suffixNew, value)
+                    childSubsting = child.children[breakbitSubstring] = _trieNode(child, suffixExisting, child.GetValue())
+                    insertionDepth += 1
                     self._nodeCount += 1
-                    self._depth = max(self._depth, deathInsetion)
+                    self._depth = max(self._depth, insertionDepth)
                     
                     return ("2 nós criados", childKey.GetSubstring(), childKey.GetValue(), childSubsting.GetSubstring(), childSubsting.GetValue(), key)
 
             i += lenSubstring
             bit = int(key[i]) if i < len(key) else None
             currentNode = child
-            deathInsetion += 1
+            insertionDepth += 1
 
             if currentNode[bit] is None:
                 currentNode.SetSubstring(substring + key[i:])
