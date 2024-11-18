5584eca98045701beaf53b86e0c6462128160a74
Erik Neves <erikrrn04@gmail.com>
2024-11-18 19:39:34 -0300

    fix read bmp

--- src/FileManager/FileManager.py
+++ src/FileManager/FileManager.py
@@ -1,34 +1,44 @@
 class FileManager:
     @staticmethod
     def ReadFile(filePath):
-        with open(f"{filePath}", "r", encoding="utf-8", errors="ignore") as file:
-            data = file.read()
-        return data
+        if filePath.lower().endswith(".bmp"):
+            with open(filePath, "rb") as file:
+                byte_data = file.read()
+            binaryString = ''.join(bin(byte)[2:].zfill(8) for byte in byte_data)
+            return binaryString
+        else:
+            with open(filePath, "r", encoding="utf-8", errors="ignore") as file:
+                data = file.read()
+            return data
 
     @staticmethod
     def SaveTextFile(filePath, content):
-        with open(f"{filePath}", "w") as file:
-            file.write(content)
+        if filePath.lower().endswith(".bmp"):
+            byte_array = bytearray()
+            for i in range(0, len(content), 8):
+                byte = content[i:i + 8]
+                byte_array.append(int(byte, 2))
+
+            with open(filePath, "wb") as file:
+                file.write(byte_array)
+        else:
+            with open(filePath, "w") as file:
+                file.write(content)
 
     @staticmethod
     def SaveBinaryFile(filePath, binaryString):
-        # Converter a string binária para uma sequência de bytes
         byte_array = bytearray()
         for i in range(0, len(binaryString), 8):
-            # Converter cada grupo de 8 bits para um byte
             byte = binaryString[i:i + 8]
             byte_array.append(int(byte, 2))
 
-        # Salvar o arquivo como binário
         with open(filePath, "wb") as file:
             file.write(byte_array)
 
     @staticmethod
     def ReadBinaryFile(filePath):
-        # Ler o arquivo binário
         with open(filePath, "rb") as file:
             byte_data = file.read()
 
-        # Converter de volta para string binária, respeitando o conteúdo original
         binaryString = ''.join(bin(byte)[2:].zfill(8) for byte in byte_data)
         return binaryString
