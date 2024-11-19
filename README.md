16335776e221f86e58deb44e9edf90124c2c5043
Gabrielzzz7 <116455055+Gabrielzzz7@users.noreply.github.com>
2024-11-19 15:02:37 -0300

    Feature/analisys (#11)
    * fix: adapts code standard
    
    * Merge branch 'main' into Feature/Analisys
    
    * feat: implementation of performance and results analysis
    
    * feat: implementation analysis of memory
    
    * add option menu
    
    * feat: add memray in README.md
    
    ---------
    
    Co-authored-by: Erik Neves <erikrrn04@gmail.com>

--- README.md
+++ README.md
@@ -7,3 +7,12 @@ Para rodar todos os testes basta executar pytest -v -s que será exibido todos o
 Para rodar um teste específico, basta executar pytest -v -s nome_do_teste.
 
 Para exibir o coverage do teste, basta executar pytest --cov --cov-report=term-missing --cov-report=html
+
+
+### Usando o memray:
+
+Para gerar um sumário no terminal, navegue até a pasta "Analysis" e execute memray summary memray_output.bin.
+
+Para gerar estatístiscas de uso de memória no terminal navegue até a pasta "Analysis" e execute memray stats memray_output.bin
+
+Para gerar um flamegraph, navegue até "Analysis" e execute: memray flamegraph memray_output.bin
--- src/main.py
+++ src/main.py
@@ -3,6 +3,11 @@ from enum import Enum
 from FileManager.FileManager import FileManager
 from LZW.Compressor import LZWCompressor
 from LZW.Decompressor import LZWDecompressor
+import cProfile
+import subprocess
+import os
+import memray
+from memray import FileDestination
 
 # Constantes
 SIGMA_SIZE = 256                # Tamanho do alfabeto utilizado
@@ -19,14 +24,25 @@ class Operation(Enum):
     def __str__(self):
         return self.value
 
+# Análise de memória
+class AnalysisType(Enum):
+    NONE = "None"
+    CPROGILE = "cProfile"
+    MEMRAY = "memray"
+
+    def __str__(self):
+        return self.value
+
 def parseArgs():
     parser = argparse.ArgumentParser(description="Aplicação para compressão e descompressão de arquivos - LZW")
 
     parser.add_argument('--max-code-bits', type=int, required=False, help='Número máximo de bits para os códigos usados.')
     parser.add_argument('--operation', type=Operation, choices=list(Operation), required=True, help='Seleção da operação de compressão ou descompressão.')
+    parser.add_argument('--analysis', type=AnalysisType, choices=list(AnalysisType), required=False, default= AnalysisType.NONE, help='Seleção da operação de compressão ou descompressão.')
+    parser.add_argument('--statistics', type=bool, required=False, default=False, help='Habilita a geração de estatísticas na compressão e descompresão.')
     parser.add_argument('--origin', type=str, required=True, help='Arquivo de origem.')
     parser.add_argument('--destiny', type=str, required=True, help='Arquivo de destino.')
-    
+
     return parser.parse_args()
 
 def ExecuteCompressOperation(origin, destiny, initialCodeLengh, maxCodeLenght, dynamic):
@@ -50,7 +66,7 @@ def main():
     args = parseArgs()
 
     if args.operation == Operation.COMPRESS_FIXED:
-        ExecuteCompressOperation(args.origin, args.destiny, DEFAULT_BITS, DEFAULT_BITS, dynamic=False)
+        ExecuteCompressOperation(args.origin, args.destiny, DEFAULT_BITS, DEFAULT_BITS, dynamic=False,)
 
     elif args.operation == Operation.COMPRESS_DYNAMIC:
         maxCodeLenght = DEFAULT_BITS if args.max_code_bits == None else args.max_code_bits
@@ -64,4 +80,36 @@ def main():
         exit(1)
 
 if __name__ == "__main__":
-    main()
+    args = parseArgs()
+    
+    if args.analysis == AnalysisType.CPROGILE:
+        analysis_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "Analysis")
+
+        if not os.path.exists(analysis_folder):
+            os.makedirs(analysis_folder)
+        
+        # Caminho completo para o arquivo de saída do cProfile
+        profile_output_file = os.path.join(analysis_folder, "profiling_result.prof")
+        
+        # Executa o perfil e salva na pasta "Analysis"
+        cProfile.run('main()', profile_output_file)
+        
+        # Executa o script de análise do perfil, que também estará na pasta "Analysis"
+        subprocess.run(["python3", os.path.join(analysis_folder, "analyze_profile.py")])
+    elif args.analysis == AnalysisType.MEMRAY:
+        # Caminho para a pasta "Analysis" no mesmo nível da pasta "src"
+        analysis_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "Analysis")
+
+        # Verifica se a pasta "Analysis" existe, se não, cria
+        if not os.path.exists(analysis_folder):
+            os.makedirs(analysis_folder)
+            
+        memray_output_file = os.path.join(analysis_folder, "memray_output.bin")
+        if os.path.exists(memray_output_file):
+            os.remove(memray_output_file)
+        with memray.Tracker(destination=FileDestination(memray_output_file)) as tracker:
+            # Executa a lógica principal do código com profiling de memória
+            main()
+
+    else:
+        main()
